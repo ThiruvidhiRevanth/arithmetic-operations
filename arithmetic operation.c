3e85298a99a48d4dd7e181b437214fa00740d1fb
Revanth <97815884+ThiruvidhiRevanth@users.noreply.github.com>
2022-01-16 14:07:05 +0530

    Add files via upload

---
+++ arithmetic operation.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+  int a,b,c;
+       printf("enter the value of a:");
+       scanf("%d",&a);
+      printf("enter the value of b:");
+     scanf("%d",&b);
+    printf("add:%d\n",c=a+b);
+     printf("sub:%d\n",c=a-b);
+    printf(" multi:%d\n",c=a*b);
+     printf("modul:%d\n",c=a%b);
+    return 0;
+} 
